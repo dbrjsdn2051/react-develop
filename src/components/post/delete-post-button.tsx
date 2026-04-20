88045960751cd9b45d703b395197d17d9b575519
dbrjsdn2051 <dbrjdn2051@naver.com>
2026-04-20 13:33:51 +0900

    feat: delete post add

---
+++ src/components/post/delete-post-button.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button.tsx";
+import { useDeletePost } from "@/hooks/mutations/post/use-delete-post.ts";
+import { useOpenAlertModal } from "@/store/alert-modal.ts";
+
+export default function DeletePostButton({ id }: { id: number }) {
+
+  const openAlertModal = useOpenAlertModal();
+
+  const { mutate: deletePost, isPending: isDeletePostPending } = useDeletePost({
+    onError: (error) => {
+      toast.error("포스트 삭제에 실패했습니다.");
+    }
+  });
+
+  const handleDeleteClick = () => {
+    openAlertModal({
+      title: "포스트 삭제",
+      description: "삭제된 포스트는 되돌릴 수 없습니다. 정말 삭제하시겠습니까?",
+      onPositive: () => {
+        deletePost(id);
+
+      }
+    });
+  };
+
+  return (
+    <Button
+      className="cursor-pointer"
+      variant={"ghost"}
+      onClick={handleDeleteClick}
+      disabled={isDeletePostPending}
+    >
+      삭제
+    </Button>
+  );
+}
